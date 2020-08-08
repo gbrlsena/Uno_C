688331638a8f78dfc4fe3547d7520047a700f6d3
Gabriel Sena <65041382+gbrlsena@users.noreply.github.com>
2020-08-07 23:41:36 -0300

    _comentado

--- Baralho_Uno.cpp
+++ Baralho_Uno.cpp
@@ -17,54 +17,57 @@ void jogar (tp_pilha *baralho, tp_pilha *mesa, Lista *jogador1,Lista *jogador2, 
 	int id = 1; 
 	int id2 = 2;
 /////////////////////////////////////////////////////////////////////////	
-	 for( ; ; ){
+	 for( ; ; ){ //for em loop
 /////////////////////////////////////////////////////////////////////////		
 	if ( vez == 1) {
 	do {
 	printf("\n");
-	tamanho(jogador1,id);
+	tamanho(jogador1,id); //a função tamanho servirá para mostrar em tela a quantidade de cartas e o id é so para mostar em tela quem ganhou
 	printf("Carta na Mesa:");
-	imprime_pilha(*mesa);
+	imprime_pilha(*mesa); // mostra em tela a carta que esta na mesa
 	printf("\n");
 	printf("Mao Jogador1:\n");
-	imprime(jogador1);
+	imprime(jogador1);  // mostra em tela as cartas que esta na mao do jogador1
 	printf("\n\nEscolha a carta: ");
 	scanf("%d " "%c" , &cor , &num); 
 	if ( num > 47 ){
 	if ( num <= 57) {
-	num = num - 48;	
+	num = num - 48;	 //Na tabela ASCII os numeros 48 a 57 são subtraídos pelo 48 para achar os numeros de 0-9 correspondentes
 		}
 	}
 	if ( num > 97 ){
-		num = num -32;
+		if ( num <= 115) {
+		num = num - 32; //Na tabela ASCII os numeros 97 a 105 são subtraídos pelo 32 para obter o alfabeto em maiusculo
+		}
 	}
 	system ("cls");
-	}while (cor > 6 && num > 10);
+	}while (cor > 6);
 	if ( num == 77 || num == 82 || num == 84 || num == 66 || num == 43|| num < 10) {
-	if ( cor == 5) {
+	if ( cor == 5) {  //ao inserir cor 5 e a letra D, o jogador irá cavar ( o D é somente por estética
 		pop(baralho,&c);
 		jogador1 = insere(jogador1,c);
 		vez++;
-	} else { 
+	} else {  //jogador não cavou, o jogo prossegue normal
 	c.cor = cor;
 	c.numero = num;
-	jogador1 = retiracarta(jogador1,c,mesa,baralho,c.cor,c.numero);	
-	jogador2 = acao(baralho,jogador2,num);
-	vez++;	
-	if ( num == 66 || num == 82){ 
-			vez = 1;
+	jogador1 = retiracarta(jogador1,c,mesa,baralho,c.cor,c.numero);	// retira a carta da mão do jogador
+	jogador2 = acao(baralho,jogador2,num); // se a carta for + (43) ou M (77), o proximo jogador cava
+	vez++;	// logo depois da jogada vez recebe +1 para ser a vez do proximo jogador
+	if ( num == 66 || num == 82){ //Na tabela ASCII, 66 e 82 são B e R, ou seja, a carta Block e Reverse
+			vez = 1; // se o jogador bloquear o proximo, ele jogda novamente ( se fosse 4 jogaodores vez receberia +2 )
 			}
-	tamanho(jogador1,id);
+	tamanho(jogador1,id); //a função tamanho servirá também para mostrar o vencedor
 	fflush(stdin);
 	system ("cls");	
 }
 } else {
-	vez = 1;
+	vez = 1; // se a carta jogada não for as predefinidas, não acontece a ação e ele cava novamente
 }
 }
 
 
-/////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////// As açoes feitas pelo jogador1 se repetem para o jagador2
+ ////////////////////////////////////////////// ( tivemos que mudar as variaveis pois estavamos tendo alguns problemas)
 	if ( vez == 2) {
 	do {
 	tamanho(jogador2,id2);
@@ -114,24 +117,23 @@ void jogar (tp_pilha *baralho, tp_pilha *mesa, Lista *jogador1,Lista *jogador2, 
 }
 }
 int main () {
-/////////////////////////////////////////////////////////////////////////	
+/////////////////////////Esses keyboards são para deixar em tela cheia//////////////////////////////
 	keybd_event ( VK_MENU, 0x36, 0, 0 ); 
 	keybd_event ( VK_RETURN, 0x1C, 0, 0 ); 
 	keybd_event ( VK_RETURN, 0x1C, KEYEVENTF_KEYUP, 0 ); 
 	keybd_event ( VK_MENU, 0x38, KEYEVENTF_KEYUP, 0 );
-/////////////////////////////////////////////////////////////////////////	
-	carta c;
+/////////////////////////Declarando as variáveis do struct, pilhas e listas////////////////////////
 	tp_pilha mesa;
 	tp_pilha baralho;
 	Lista* jogador1;
 	Lista* jogador2;
 	int vez;
-/////////////////////////////////////////////////////////////////////////		
+/////////////////////////////////////////Logo e menu///////////////////////////////////////////////				
 char opc=1;
 
 printf("\n\n");
 gotoxy(65,22);
-printf("      VCulture Dev. \n");
+printf("      VCulture Dev. \n"); 
 Sleep(1000);
 gotoxy(65,23);
 printf("       Apresenta \n");
@@ -149,7 +151,6 @@ Sleep(600);
 printf("*****");
 Sleep(200);
 system("cls");
-for ( int j = 1 ; j < 2 ; j++){
 do {
 printf("\n\n");
 gotoxy(55,19);
@@ -169,51 +170,47 @@ printf("       `Y88888P' dP     dP  `8888P'   \n");
 gotoxy(55,26);
 printf("       oooooooooooooooooooooooooooooo      Escolha:");                   
 scanf("%c", &opc);
-
+//////////////////////////////Seleção do menu///////////////////////////////////////
 switch (opc) {
-	case 49 : {
-		system("cls");
-		inicializa_pilha(&mesa);
-		criar_baralho(&baralho);
-	 	carta_mesa(&mesa,&baralho);	
-		jogador1 = criar_mao(&baralho,jogador1);
-		jogador2 = criar_mao(&baralho,jogador2);
+	case 49 : { //Numeros 49-52 correspondentes na tabela ASCII a 1-4
+		system("cls");  
+		criar_baralho(&baralho); //criando o baralho passando a pilha de variável baralho
+	 	carta_mesa(&mesa,&baralho);	 //criando a carta que aparecerá na mesa no início da partida
+		jogador1 = criar_mao(&baralho,jogador1); //cria as cartas para o jogador 1
+		jogador2 = criar_mao(&baralho,jogador2); //cria as cartas para o jogador 2
 		printf("\n");
-		jogar (&baralho,&mesa,jogador1,jogador2,vez);
+			imprime_pilha(&baralho); 
+}
+		jogar (&baralho,&mesa,jogador1,jogador2,vez); //função jogar passando o baralho, a mesa e os jogadores
 		Sleep(3000);
 		break;
 	}
 	case 50: {
 		system("cls");
-		manual(opc);
+		manual(opc); // chama a função Manual ( esta em função para fins de organização na main )
 		system("PAUSE");
 		system("cls");
-		j--;
 		break;
+	}
+	case 51: {
+		system("cls");
+		creditos(opc); // chama a função Creditos ( esta em função para fins de organização na main )
+		system("cls");
+
 	}
 	case 52: {
 		system("cls");
-		return 0;
+		return 0; // fecha o jogo
 		break;
 	}
-		case 51: {
-		system("cls");
-		creditos(opc);
-		system("cls");
-		j--;
-		//return 0;
-		//break;
-	}
+
 	default: {
 		printf("  Erro: Escolha uma opcao valida");
 		Sleep(500);
 		system("cls");
-	//	system("exit");
 	}
 	
 }
 } while (opc != 52);
 }
-}
-
 
